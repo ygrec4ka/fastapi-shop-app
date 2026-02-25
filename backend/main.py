f382e1ed5eb5f743a64d7b2060618323c1996a03
ygrec4ka <sam06ser@mail.ru>
2026-02-25 14:47:36 +0400

    create base fastapi app

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+from app import settings
+
+app = FastAPI()
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host=settings.run.host,
+        port=settings.run.port,
+    )
