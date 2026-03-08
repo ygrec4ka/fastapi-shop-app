66c842ccb566966d008c891fbef9395b3698905f
ygrec4ka <sam06ser@mail.ru>
2026-03-08 18:45:54 +0400

    include cart router

--- backend/app/api/api_v1/__init__.py
+++ backend/app/api/api_v1/__init__.py
@@ -5,6 +5,7 @@ from app.config import settings
 
 from .categories import router as categories_router
 from .products import router as products_router
+from .cart import router as cart_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
@@ -13,3 +14,4 @@ router = APIRouter(
 
 router.include_router(categories_router)
 router.include_router(products_router)
+router.include_router(cart_router)
