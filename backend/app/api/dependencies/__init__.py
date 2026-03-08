b87c49e23a9f20709ebe72a005fcbf3282100330
ygrec4ka <sam06ser@mail.ru>
2026-03-08 18:17:19 +0400

    init product/cart service to import

--- backend/app/api/dependencies/__init__.py
+++ backend/app/api/dependencies/__init__.py
@@ -1,5 +1,9 @@
 from .category import get_category_service
+from .product import get_product_service
+from cart import get_cart_service
 
 __all__ = [
     "get_category_service",
+    "get_product_service",
+    "get_cart_service",
 ]
