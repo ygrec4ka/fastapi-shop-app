f034e981bfe748e8eba10a7ad636158431e6c35c
ygrec4ka <sam06ser@mail.ru>
2026-03-30 23:57:43 +0400

    refactor: transport fastapi_users.py to dependency folder

--- src/backend/api/dependencies/authentication/fastapi_users.py
+++ src/backend/api/dependencies/authentication/fastapi_users.py
@@ -1,7 +1,7 @@
 from fastapi_users import FastAPIUsers
 
 from backend.core.authentication.backend import authentication_backend
-from backend.api.dependencies.authentication.user_manager import get_user_manager
+from .user_manager import get_user_manager
 
 from backend.core.types import UserIdType
 from backend.core.models import User
